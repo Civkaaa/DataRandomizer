40eddb9e0727c8bba59aa3f42f6be76012309b7f
unknown <poka132@yandex.ru>
2023-09-29 01:46:42 +0300

    Issues 1-5

--- DGcopy.py
+++ DGcopy.py
@@ -7,8 +7,15 @@ from os import getcwd
 from time import time
 # colors and fonts 
 
+roboto6 = ('roboto', '6')
+roboto7 = ('roboto', '7')
+roboto8 = ('roboto', '8')
+roboto10 = ('roboto', '10')
+roboto12 = ('roboto', '12')
+roboto16 = ('roboto', '16')
+roboto18 = ('roboto', '18')
 
-
+colorsdict = {'gray' : '#e7edf9', 'green' : '#00c3a3', 'darkgreen' : '#004a46', 'lightgray' : '#8f8f9d', 'darkblue' : '#1e325a', 'lightblue' : '#63a8e4'}
 
 
 d = getcwd() # current directory
@@ -218,6 +225,11 @@ def Datatype1():
     applybtn = tk.Button(mainpanel, height=1, width=10, text='сохранить', font=roboto8, command=uniqueinput, bd=0, activeforeground=colorsdict['darkgreen'],
                          background=colorsdict['green'], relief='flat', activebackground=colorsdict['green'], foreground=colorsdict['darkgreen'])
     linestrack = IntVar()
+    applybtn.place(x=500, y=30)
+    uniquelines.place(x=300, y=8)
+    one.place(x=10, y=30)
+    two.place(x=10, y=55)
+    three.place(x=10, y=80)
     one = tk.Radiobutton(mainpanel, text='одно число', variable=linestrack, value=1, background='white', font=roboto8)
     two = tk.Radiobutton(mainpanel, text='пары чисел', variable=linestrack, value=2, background='white', font=roboto8)
     three = tk.Radiobutton(mainpanel, text='тройки чисел', variable=linestrack, value=3, background='white', font=roboto8)
@@ -376,7 +388,9 @@ def dirpick():
 
 
 main = tk.Tk()
-
+main.title('Data Generator')
+main.geometry('850x550+300+200')
+main.configure(background=colorsdict['gray'])
 datatypelb = tk.Label(main, text='Тип данных', font=roboto16, background='#e7edf9')
 datatype = ttk.Combobox(main, state='readonly', values=['Набор чисел из N строк', 'Строка из N символов'], width=50)
 datatype.bind("<<ComboboxSelected>>", Dataselection)
